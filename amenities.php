2ce98e29bea48915f861ffec4fd04c0ebabba543
jmaple4 <jmaple4@gmail.com>
2014-10-08 22:08:34 -0700

    more changes to check in

---
+++ amenities.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>solr</title>
+    <?php include 'assets/includes/head.php';?>
+</head>
+
+<body class="page-amenities">
+    <?php include 'assets/includes/nav.php';?>
+
+   
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                 <div class="heroBlock">
+                    <img class="pageHeroImage img-responsive" src="assets/img/photos/amenities-hero.jpg" width="1024" height="263" alt="garden">
+                    <span class="logoBranding"></span>
+                     </div><!-- /heroBlock -->
+                </div>
+                <!-- /col -->
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /container -->
+   
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>We offer a lot of amazing amenitites</h1>
+            </div>
+        </div>
+        <!-- /row -->
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="media">
+                    <i class="pull-left icon-wifi"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Free Hi Speed WiFi</h4>
+                        <p>Although a look back at an era and philosophy in time gone by, the Summer of LOVE is forward thinking when it comes to technology. We provide our guest with a 5 Star, Hi Technology driven WiFi Connection supported by some of the best Engineer Artist behind these systems in the Silicon Valley and San Francisco Bay area. We’ve spent nearly $50,000 to bring this service to the building and $20,000 a year making it hi-octane. The System is a SG200-26P Gigabit Backbone Switch with 26 Gigabit Ethernet ports, including 12 Power Over Ethernet ports. (POE) It boasts this high speed backplane which can handle dozens of 1 Gigabit communication sessions between devices simultaneously with Ubiquity UniFi AP, Dual-Band 802.11n access points installed strategically throughout the building.  These dual-band (2.4Ghz and 5Ghz) access points are centrally managed, monitored and tested to provide from 100Mbps to 450Mbps of connection speed. </p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-housekeeping"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Free Housekeeping &amp; Bedding Service</h4>
+                        <p>The Summer of LOVE provides FREE, a bi-weekly linen change, vacuuming and cleaning of all its guest beds, rooms and restroom facilities. It’s cool if you come prepared to do your part but to help you our housekeeping staff provides this wonderful Free service for you. So make sure you say “Hi” and “Thanks”…. New Sheets and Pillow cases….Cool man.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-measuring-cup"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Communal Kitchen &amp; kitchenettes</h4>
+                        <p>The Summer of LOVE provides shared access to a fully stocked communal kitchen. The facility is equipped with 2 full sized stove top range over ovens, 2 microwave ovens, mini refrigerator, stainless steel sink and dishwasher. A gathering place, the kitchen is a slice of home with the aroma of good food from around the world. Don’t feel like going to the kitchen for water? There is a filter purified water supply faucet in each kitchenette located on every floor.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-leaf"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">SOL Garden and Hibachi Grill</h4>
+                        <p>The Summer of LOVE recognizes how important the outdoors can be. Sometimes finding a peaceful outdoor space is hard to do. But the SOL Garden is a private outdoor space reserved for our guest to enjoy reading, cooking, gathering or just taking in fresh air. A great common area space the SOL Garden is like finding a jewel in a treasure chest. Way cool...</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-bicycle"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Bike Rack</h4>
+                        <p>The Summer of LOVE advocates the use of bicycles and provides its guest with a great and safe place to park their bikes just feet from the building entrances. Whether you’re storing it for a few months or need it every day, our unique bike rack has a spot for your transportation at no additional cost. Really cool man...</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-lock"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Private Gated Entry</h4>
+                        <p>The Summer of LOVE is cool and we want to be safe. For your safety and the safety of all guest, the SOL has a locked entry gate and Garden Cottages Access door that is for the exclusive use of its guest. Pass key technology that is not easily duplicated protects the house from those unauthorized. Each individual guest is issued a personal identification and pass key for their exclusive use and access. It’s cool...</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-calendar"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Kitchens Showers and Bathroom Cleaned Daily</h4>
+                        <p>At the Summer of LOVE, we believe that cleanliness is next to, well happiness. Our incredible housekeeping staff works hard to maintain the building and its facilities clean and disinfected. We recognize the need for sanitary conditions and spare no time ensuring all surfaces are cleaned with the best green commercial cleaning products. You can rest assured the Summer of LOVE is as clean as it is cool.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+            </div><!-- /col-6 -->
+
+
+
+
+
+            <div class="col-sm-6">
+                <div class="media">
+                    <i class="pull-left icon-coffee"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Free Trieste Coffee and Cappucino</h4>
+                        <p>At the Summer of LOVE, you’ll get by with a little help from your friends and a FREE cup of Caffe Trieste Coffee. We proudly bring to you and your attention this preeminent cup of Superb Coffee by San Francisco’s own Caffe Trieste. CAFFE TRIESTE was the first place for Espresso on the West Coast back in the 50's and it’s now an International landmark in San Francisco's North Beach.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-microphone"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Access to 18 Rehearsal Studios and Event Space at Music City San Francisco Nearby</h4>
+                        <p>The Music City San Francisco rehearsal Studios is already a cool Venue, Event &amp; Academy sight, a place for hobbyist, students learning a new instrument or working professional musician rehearsing for their next gig, but it’s being built to be much more. Already a place with 18 well equipped rehearsal studios to handle your needs, it promises to be the place for Musicians and Artist alike to display, practice and learn more about their craft and their art. Due to be completed in 2015.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                     <img src="assets/img/light-power.svg" class="pull-left" width="42" height="48" />
+                    <div class="media-body">
+                        <h4 class="media-heading">Free Utilities and Purified Drinking Water</h4>
+                        <p>At the Summer of LOVE you’ll never get a utility bill. Electricity, Heating, Trash, Water and  Speedy WiFi are all Free.  We also provide filter purified drinking water on tap that always taste clean and crisp.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-coffee"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Free Hot Chocolate</h4>
+                        <p>Just when you thought it couldn’t get any better, top it off with a cup of Hot Chocolate  anytime on the Summer of Love. We provide our guest with some really cool freebies but this one is hot. Enjoy, it’s our pleasure. </p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-smile-o"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Common Area Lounge</h4>
+                        <p>The Summer of LOVE living room lounge is a great place for eating, reading, visiting, or just hanging out. This popular gathering spot always seems to have a few people to keep you company. With a large window that faces San Francisco’s famed Post Street the living the common area lounge seconds as the occasional music room and party place. Bring your acoustical instrument or dinner plate, you can always find what’s happening here. So cool...</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-camera"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Common Area Camera System</h4>
+                        <p>Safe as it may be, the Summer of LOVE has surveillance cameras throughout the common areas for additional safety and protection that are each recorded to a hard drive and accessible remotely. In addition to the common area surveillance the main entrance gate and Cottage Garden entrance from Post Street are each on camera and recorded to hard drive 24/7/365.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-bus"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Bus Stop and Public Transportation</h4>
+                        <p>Busses, shuttles and Taxis are right at your door step. As if  there just for you, access to the #2 Clement, #3Jackson, #27Bryant, #38Geary, #38L Geary Limited, #19Polk, Post &amp; Hyde, Leavenworth &amp; Post, Geary &amp; Leavenworth, Muni Bus Routes along with the Academy of Art University Shuttle all stop just a few feet from your front door.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+                <div class="media">
+                    <i class="pull-left icon-cube"></i>
+                    <div class="media-body">
+                        <h4 class="media-heading">Mini Refrigerator in every room</h4>
+                        <p>SOL recognizes the need for keeping some food on hand. We provide our guest with a mini fridge in each room for your convenience. Beverages and other perishable are better kept in your room.</p>
+                    </div> <!-- /media-body -->
+                </div><!-- /media -->
+            </div><!-- /col-6 -->
+        </div>
+        <!-- /row -->
+    </div>
+    <!-- /.container -->
+    <?php include 'assets/includes/footer.php';?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>solr</title>
+    <?php include 'assets/includes/head.php';?>
+</head>
+
+<body class="page-home">
+    
+    <?php include 'assets/includes/nav.php';?>
+
+    <?php include 'assets/includes/slider-2.php';?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 homeAmentitySpotlight">
+                <h1>Amenities We Offer</h1>
+                <div class="col-sm-6">
+                    <div class="media">
+                        <i class="icon-wifi pull-left"></i>
+                        <div class="media-body">
+                            <h4 class="media-heading">Free Hi<br />Speed WiFi</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                    <div class="media">
+                        <i class="icon-coffee pull-left"></i>
+                        <div class="media-body">
+                            <h4 class="media-heading">Free Caffe Trieste<br />Coffee &amp; Espresso</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                    <div class="media">
+                        <i class="icon-housekeeping pull-left"></i>
+                        <div class="media-body">
+                            <h4 class="media-heading">Free Housekeeping <br />&amp; Bedding Change</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                </div><!-- /col-6 child -->
+
+                <div class="col-sm-6">
+                    <div class="media">
+                        <img src="assets/img/light-power.svg" class="pull-left" width="42" height="48" />
+                        <div class="media-body">
+                            <h4 class="media-heading">Free Utilities &amp; 
+                            <br />Purified Drinking Water</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                    <div class="media">
+                        <i class="icon-measuring-cup pull-left"></i>
+                        <div class="media-body">
+                            <h4 class="media-heading">Communal<br />Kitchen</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                    <div class="media">
+                        <i class="icon-microphone pull-left"></i>
+                        <div class="media-body">
+                            <h4 class="media-heading">Access to Music<br />City Rehersal</h4>
+                        </div> <!-- /media-body -->
+                    </div><!-- /media -->
+                </div><!-- /col-6 child -->
+
+            </div><!-- /col-6 parent -->
+            <div class="col-sm-6">
+                <img src="assets/img/photos/triple-room.jpg" width="507" height="337" class="img-responsive featureImg" title="Triple Room" />
+            </div>
+            <!-- /col-6 -->
+        </div>
+        <!-- /row -->
+        <div class="row">
+            <div class="col-sm-6">
+                <img src="assets/img/photos/kitchen.jpg" width="500" height="333" class="img-responsive featureImg" title="Kitchen" />
+            </div>
+            <!-- /col-6 -->
+            <div class="col-sm-6 moreAmenityList">
+                <h1>More Amenities</h1>
+                <ul>                
+                <li>Tropical Garden with small barbecue</li>
+                <li>Free Hot chocolate</li>
+                <li>Secure bike rack</li>
+                <li>Common Area Lounge</li>
+                <li>Private Gated Entry</li>
+                <li>Recorded Camera Security System throughout common areas</li>
+                <li>Bathrooms and kitchen cleaned daily</li>
+                <li>Mini Refrigerator in each room</li>
+                <li>Bus Stop and AAU Passenger Shuttle at your door step</li>
+                </ul>
+
+                <a href="amenities.php" class="btn-primaryAction pull-right">Amenities Page</a>
+                <span class="pull-right buttonTag">Check out the Full List</span>
+            </div>
+        </div>
+        <!-- /row -->
+        <div class="row">
+            <div class="col-sm-6 gardenSection">
+                <h1>Garden Cottages</h1>
+                <p>Each private entrance way steps off from a European styled walkway which leads from the Secured. Private Entrance from Post Street. The all new “Cottages” were completed in Mid-2014 and built to spec from the ground up.</p> 
+                <p>Each of the 4 Private Units have their own:</p>
+                <ul class="clearfix">
+                <li>Complete Kitchen w/ new full sized appliances</li>
+                <li>Refrigerator</li>
+                <li>Range with microwave</li>
+                <li>Dishwasher</li>
+                <li>Bathtub and Shower</li>
+                <li>Storage closets</li>
+                <li>and Cool Design</li>
+                </ul>
+
+                <a href="#" class="btn-primaryAction">Call for Availability</a>
+            </div>
+            <div class="col-sm-6">
+                <img src="assets/img/photos/common-room.jpg" width="500" height="333" class="img-responsive featureImg" title="Common Room" />
+            </div>
+            <!-- /col-6 -->
+        </div>
+        <!-- /row -->
+
+    </div>
+    <!-- /.container -->
+    <?php include 'assets/includes/footer.php';?>
+</body>
+
+</html>
